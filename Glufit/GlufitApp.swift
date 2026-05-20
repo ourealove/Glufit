18b3b82f2ddfb7395b50a1a5533721ce538a870e
JHSung <tjdwnsgud22@naver.com>
2026-05-20 15:37:21 +0900

    Initial Commit

---
+++ Glufit/GlufitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlufitApp.swift
+//  Glufit
+//
+//  Created by ourealove on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GlufitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
